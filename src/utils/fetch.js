599bc12e975e4ce3e8d8de4271b2f993a4c20c03
jorgeloaiza48 <jelm48@misena.edu.co>
2024-10-06 17:05:22 -0500

    creando la petición

---
+++ src/utils/fetch.js
@@ -0,0 +1,14 @@
+const API_KEY = 'sZaheOUdKCLArKMOTa6oFv4o2uTzvKl4iWRemyyT';
+const API_URL = 'https://api.nasa.gov/planetary/apod';
+
+export default async (urlParams, string) => {
+    try{
+        const response = await fetch(`${API_URL}?api_key=${API_KEY}${typeof urlParams !== 'undefined' && urlParams.length > 0 ? urlParams:''}`            
+        );
+        const data = await response.json();
+        return Promise.resolve(data);
+        
+    } catch (error){
+        return Promise.reject(error)
+    }
+}
--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,8 +1,20 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { View,StyleSheet } from 'react-native'
 import Header from '../../components/Header'
+import fetchApi from '../../utils/fetch'
 
 function Home() {
+    useEffect(()=>{
+        const loadTodaysImage = async () => {
+            try {
+                const loadTodaysImage = await fetchApi();
+                console.log(loadTodaysImage)
+            } catch (error) {
+                console.log(error)
+            }
+        }
+        loadTodaysImage().catch(null)
+    },[])
     return (
         <View style={styles.container}>
             <Header />
@@ -15,6 +27,8 @@ export default Home
 const styles = StyleSheet.create({
 container:{
     flex:1,
-    paddingHorizontal:16,
+    paddingHorizontal:18,
+    paddingTop:55
 }
 })
+
