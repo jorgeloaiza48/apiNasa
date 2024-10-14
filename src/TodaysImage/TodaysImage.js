3ed17e4aec569c0303c546847456982cb1a6368a
jorgeloaiza48 <jelm48@misena.edu.co>
2024-10-14 17:07:56 -0500

    implementando el componente  la imagen del día

---
+++ src/TodaysImage/TodaysImage.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import { View, Text, StyleSheet, Image, Button } from 'react-native'
+
+export default function TodaysImage({ date, title, url }) {
+    return (
+        <View style={styles.container}>
+            <Image source={{ uri: url }} style={styles.image} />
+            <Text style={styles.title}>{title}</Text>
+            <Text style={styles.date}>{date}</Text>
+            <View style={styles.buttonContainer}>
+                <Button title="View" />
+            </View>
+        </View>
+    )
+}
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: '#2c449d',
+        marginVertical: 16,
+        marginHorizontal: 24,
+        borderRadius: 32,
+        padding: 16
+    },
+    imageContainer: {
+        borderWidth: 2,
+        borderRadius: 32,
+    },
+    image: {
+        width: '100%',
+        height: 190,
+        borderWidth: 2,
+        bordercolor: '#fff',
+        borderRadius: 32,
+    },
+    title: {
+        color: '#fff',
+        fontSize: 20,
+        marginVertical: 12,
+        fontWeight: 'bold',
+    },
+    date: {
+        color: '#fff',
+        fontSize: 16,
+    },
+    buttonContainer: {
+        alignItems: 'flex-end'
+    }
+})
+
--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,23 +1,29 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { View,StyleSheet } from 'react-native'
 import Header from '../../components/Header'
 import fetchApi from '../../utils/fetch'
+import TodaysImage from '../../TodaysImage'
 
 function Home() {
+    const [todaysImage, setTodaysImage] = useState([])
     useEffect(()=>{
         const loadTodaysImage = async () => {
             try {
-                const loadTodaysImage = await fetchApi();
-                console.log(loadTodaysImage)
+                const todaysImageResponse = await fetchApi();
+                setTodaysImage(todaysImageResponse)               
             } catch (error) {
                 console.log(error)
+                setTodaysImage(undefined)
             }
         }
         loadTodaysImage().catch(null)
     },[])
+
+    console.log(todaysImage)
     return (
         <View style={styles.container}>
             <Header />
+            <TodaysImage  url={todaysImage.url} date={todaysImage.date} title={todaysImage.title}/>
         </View>
     )
 }
